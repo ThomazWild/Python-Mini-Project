db016c40f15f9d11e5b3411a0278897a19a3606d
Thomaz <50690864+ThomazWild@users.noreply.github.com>
2019-05-24 23:46:11 +0530

    Add files via upload

---
+++ CAPITALIZE.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 17:02:31 2019
+
+@author: Thomaz
+"""
+
+STDIN = input('Enter a string: ')
+STDIN = STDIN.lower()
+STDIN = STDIN.title()
+print(STDIN)
